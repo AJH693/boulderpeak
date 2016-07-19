322bbf292af96a26342c55d47e55143ec3f245f5
Hartman, Andrew <hartman.andrew@principal.com>
2016-07-19 13:20:25 -0500

    test

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,13 +4,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 import hello.views
+import polls.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
+    url(r'^$', polls.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls'))
