f792b8f7df5ebfdc1152270b819d2eda760bb532
Hartman2 <Hartman2@iastate.edu>
2016-07-20 22:50:43 -0500

    removing unnecessary urls

--- hartmanhome/urls.py
+++ hartmanhome/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^about_me/$', views.about_me, name='about_me'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+	url(r'^about_me/$', views.about_me, name='about_me')
 ]
